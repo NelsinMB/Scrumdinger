5c10a11e9cf872d50f5ede8b2c88f5636229aaa3
Nelsin Martin-Burnett <nelsinmburnett@gmail.com>
2023-08-02 17:31:02 -0400

    SwiftUI essentials, Using stacks to arrange views, complete.

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -14,18 +14,31 @@ struct MeetingView: View {
             HStack {
                 //What is leading alignment? What is trailing alignment?
                 VStack(alignment: .leading) {
-                    Text("Seconds Elapsed")
+                    Text("Seconds Elapsed").font(.caption)
                     Label("300", systemImage: "hourglass.tophalf.fill")
                     //Where does the hourglass come from?
                 }
                 Spacer()
                 VStack (alignment: .trailing) {
-                    Text("Seconds Remaining")
+                    Text("Seconds Remaining").font(.caption).bold()
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                     //Where does the hourglass come from?
                 }
             }
-        }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutesw")
+            Circle().strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next speaker")
+            }
+        }.padding()
+        
     }
 }
 
